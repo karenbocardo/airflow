15f517b8dfec1f977a2bdc0ba1a389eb42b9c79b
karenbocardo <karenbocardo@icloud.com>
2023-03-07 15:28:03 -0600

    fix dag creation schedule interval

--- dags/jobs_dag.py
+++ dags/jobs_dag.py
@@ -6,13 +6,13 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 
 config = {
-    'dag_id_1': {'schedule_interval': "@daily", 
-                "start_date": datetime(2018, 11, 11),
+    'dag_id_1': {'schedule_interval': None, 
+                "start_date": datetime(2022, 11, 11),
                 "table_name": "table_name_1"},  
-    'dag_id_2': {'schedule_interval': "@daily", 
+    'dag_id_2': {'schedule_interval': None, 
                 "start_date": datetime(2018, 11, 11),
                 "table_name": "table_name_2"},  
-    'dag_id_3':{'schedule_interval': "@daily", 
+    'dag_id_3':{'schedule_interval':  None, # changed from "@daily"
                 "start_date": datetime(2018, 11, 11),
                 "table_name": "table_name_3"}
     }
@@ -21,7 +21,7 @@ def log_information(dag_id, database):
     logging.info(f"{dag_id} start processing tables in database: {database}")
 
 for id, dict in config.items():
-    with DAG(id, start_date=dict["start_date"]) as dag:
+    with DAG(id, start_date=dict["start_date"], schedule_interval=dict["schedule_interval"]) as dag:
         log_info = PythonOperator(
             task_id="log_info",
             python_callable=log_information,
