ecc5ba7c532a3446f58828aff6b99149fe075a87
karenbocardo <karenbocardo@icloud.com>
2023-03-08 16:48:27 -0600

    code practice: refactor dynamic DAG creation and add branching

--- dags/jobs_dag.py
+++ dags/jobs_dag.py
@@ -4,32 +4,51 @@ import logging
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python_operator import BranchPythonOperator
 
 config = {
     'dag_id_1': {'schedule_interval': None, 
-                "start_date": datetime(2022, 11, 11),
-                "table_name": "table_name_1"},  
+                'start_date': datetime(2022, 11, 11),
+                'table_name': 'table_name_1'},  
     'dag_id_2': {'schedule_interval': None, 
-                "start_date": datetime(2018, 11, 11),
-                "table_name": "table_name_2"},  
-    'dag_id_3':{'schedule_interval':  None, # changed from "@daily"
-                "start_date": datetime(2018, 11, 11),
-                "table_name": "table_name_3"}
+                'start_date': datetime(2018, 11, 11),
+                'table_name': 'table_name_2'},  
+    'dag_id_3':{'schedule_interval':  None, # changed from '@daily'
+                'start_date': datetime(2018, 11, 11),
+                'table_name': 'table_name_3'}
     }
 
+# branching function
+def check_table_exist():
+    ''' method to check that table exists '''
+    if True:
+        return 'insert_row'
+    return 'create_table'
+
 def log_information(dag_id, database):
-    logging.info(f"{dag_id} start processing tables in database: {database}")
+    logging.info(f'{dag_id} start processing tables in database: {database}')
 
 for id, dict in config.items():
-    with DAG(id, start_date=dict["start_date"], schedule_interval=dict["schedule_interval"]) as dag:
-        log_info = PythonOperator(
-            task_id="log_info",
+    with DAG(id, start_date=dict['start_date'], schedule_interval=dict['schedule_interval']) as dag:
+        start = PythonOperator(
+            task_id='print_process_start',
             python_callable=log_information,
-            op_kwargs={"dag_id": id, "database": "example"}
+            op_kwargs={'dag_id': id, 'database': 'example'}
         )
 
-        insert = DummyOperator(task_id="insert_row")
+        check = BranchPythonOperator(
+            task_id='check_table_exists',
+            python_callable=check_table_exist
+        )
 
-        query = DummyOperator(task_id="query_the_table")
+        create = DummyOperator(task_id='create_table')
 
-        log_info >> insert >> query
+        insert = DummyOperator(task_id='insert_row', 
+                               trigger_rule='none_failed')
+
+        query = DummyOperator(task_id='query_table')
+
+        start >> check
+        check >> [create, insert]
+        create >> insert
+        insert >> query
